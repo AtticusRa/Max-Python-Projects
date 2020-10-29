37df4cfdd09e617bbec268b3f4e4be4ef2b04ca8
AtticusRa <max.situs@gmail.com>
2020-10-29 00:40:44 -0700

    message

--- basicBudget.py
+++ basicBudget.py
@@ -3,30 +3,42 @@
 ##Basic Budgeting app.
 """ Takes in user input and offers basic advice based on what type of budget you would like to have,
 if your spending is extreme enough it puts out a warning and new budget. """
+"""
+TODO:
+DONE: Use dictionaries to create spending and income categories
+Create functions that populate category dictionaries based on user input until user is done. 
+Use pre selected designations like housing, rent, food, etc.
+Create pretty output of end budget, export to csv file?
+Create risky and safe budget algorithm
+Graphical output to better visualize the budget breakdown
+
+
+"""
 
 import re, math
 
 
 def main():
-    newBudget = budget.startAcct()
-    newBudget.checkAcct()
+    monthlyIncome = int(input('How much is your monthly income? '))
+    monthlyExpense = int(input('How much are you spending every month? '))
+    newBudget = budget(monthlyIncome, monthlyExpense, 'Safe')
     newBudget.updateBudget()
+    budget.incomeCategories(newBudget)
+    budget.spendingCategories(newBudget)
+    print(newBudget.incomeCategories)
+    print(newBudget.spendingCategories)
 
 
 class budget():
-    def __init__(self, monthlyIncome, monthlyExpense, spendingCategories,
-                 incomeCategories):
-        self.monthlyIncome = monthlyIncome
-        self.monthlyExpense = monthlyExpense
-        self.spendingCategories = spendingCategories
-        self.incomeCategories = incomeCategories
-
-    @classmethod
-    def startAcct(cls):
-        monthlyIncome = int(input('How much is your monthly income? '))
-        monthlyExpense = int(input('How much are you spending every month? '))
-        newBudget = budget(monthlyIncome, monthlyExpense, 1, 1)
-        return newBudget
+    def __init__(self, monthlyIncome, monthlyExpense, budgetStyle):
+        self.monthly = {}
+        self.incomeCategories = {}
+        self.spendingCategories = {}
+        self.monthly['Monthly Income'] = monthlyIncome
+        self.monthly['Monthly Expense'] = monthlyExpense
+        self.budgetStyle = budgetStyle
+        self.incomeCategories['W2'] = 0
+        self.spendingCategories['Food'] = 0
 
     def incomeSources(self):
         pass
@@ -34,25 +46,43 @@ class budget():
     def expenseSources(self):
         pass
 
-    def checkAcct(self):
-        if self.monthlyExpense > self.monthlyIncome:
-            print(
-                'You are spending more than you are currently making. That\'s ok, let\'s suggest a good budget.'
-            )
-        else:
-            print(r"You're not in the red. Let's make it even better.")
-
     def updateBudget(self):
         choice = str(
-            input(
-                'Would you like a Safe or Risky budget?\nA riskier budget will allow you to spend more but you will have less leftover for a rainy day. \nWhereas a safer budget will afford you less big expenses, but greater peace of mind.\n'
-            ))
+            input('''Would you like a Safe or Risky budget?
+                \nA riskier budget will allow you to spend more but you will have less leftover for a rainy day.
+                \nWhereas a safer budget will afford you less big expenses, but greater peace of mind.
+                \nBudget Selection: '''))
         if re.search('(S|s)afe', choice):
-            print('safe')
+            print(
+                'You have selected a Safe Budet style. Updating your account now.'
+            )
+            self.safeBudget()
         elif re.search('(R|r)isky', choice):
-            print('risky')
+            print(
+                'You have selected a Risky Budget style. Updating your account now.'
+            )
+            self.riskyBudget()
+            self.budgetStyle = 'Risky'
         else:
-            print('fail')
+            print(
+                'You did not enter a valid budget stlye. You have defaulted to a safe style.'
+            )
+            self.safeBudget()
+
+    def incomeCategories(self):
+        pass
+
+    def spendingCategories(self):
+        pass
+
+    def riskyBudget(self):
+        pass
+
+    def safeBudget(self):
+        pass
+
+    def displayBudget(self):
+        pass
 
 
 main()
---
+++ classtest.py
@@ -0,0 +1,15 @@
+class Test():
+    sampleDict = {'1': True}
+
+    def __init__(self, income, expense, value=None):
+        self.monthly = {}
+        self.monthly['Income'] = income
+        self.monthly['Expense'] = expense
+        self.value = value
+
+
+t = Test(100, 900)
+print(t.monthly)
+t.monthly['Woke status'] = 'not woke'
+print(t.monthly)
+print(t.sampleDict)
